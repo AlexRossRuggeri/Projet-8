1604564762ea77ac7a4c9f20cffa8d46aa791a7a
Alexandre Ross-Ruggeri <alexandre.rossruggeri@gmail.com>
2025-07-26 20:52:05 +0200

    Add Error page component and styles
    Introduces a new Error page (404) with a simple message and a link to return to the homepage. Includes the initial SCSS file for styling.

---
+++ src/pages/Error/Error.tsx
@@ -0,0 +1,18 @@
+import { Link } from 'react-router-dom';
+import './Error.scss';
+
+function Error() {
+  return (
+    <div className="error-section">
+      <h2 className="error-section__title">404</h2>
+      <p className="error-section__text">
+        Oups! La page que vous demandez n'existe pas.
+      </p>
+      <Link className="error-section__link" to="/">
+        Retourner sur la page d'accueil
+      </Link>
+    </div>
+  );
+}
+
+export default Error;
