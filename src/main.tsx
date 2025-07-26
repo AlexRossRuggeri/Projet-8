57d02c7ddeed41598e6f78bb2e71c9f6aaa108bd
Alexandre Ross-Ruggeri <alexandre.rossruggeri@gmail.com>
2025-07-26 20:52:36 +0200

    Add main.scss and set up React Router in main.tsx
    Introduced a new main.scss file for global styles and refactored main.tsx to use React Router for routing between Home, About, Projects, and Error pages. Updated imports and replaced the previous App component rendering with a layout-based route structure.

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,27 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import Layout from './layout';
+import Home from './pages/Home/Home';
+import Error from './pages/Error/Error';
+import About from './pages/About/About';
+import Projects from './pages/Projects/Projects';
+
+import './index.css';
+
+const root = createRoot(document.getElementById('root')!);
+root.render(
+  <React.StrictMode>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="/a-propos" element={<About />} />
+          <Route path="/projects/:id" element={<Projects />} />
+          <Route path="*" element={<Error />} />
+        </Route>
+      </Routes>
+    </Router>
+  </React.StrictMode>,
+);
