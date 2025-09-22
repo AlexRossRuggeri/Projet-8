6e8f1978514835faa3a4beaffee7b58d46d75108
Alexandre Ross-Ruggeri <alexandre.rossruggeri@gmail.com>
2025-09-22 12:10:58 +0200

    chore: init portfolio (Vite + React + TS + Tailwind)

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="min-h-screen grid place-items-center">
+      <h1 className="text-3xl font-bold underline">Tailwind OK ✅</h1>
+    </div>
+  );
+}
+
+export default App;
