e7ba04a16f238555deeb8135c5d9e6884c3fe3f2
Alexandre Ross-Ruggeri <alexandre.rossruggeri@gmail.com>
2025-10-20 12:51:50 +0200

    Add React Router and restructure app entry
    Introduces React Router for client-side routing, adds Layout, Home, About, ProjectFile, and Error pages, and updates the main entry to use route-based rendering.

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,27 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import Layout from './components/Common/Layout/Layout';
+import Home from './pages/Home/Home';
+import Error from './pages/Error/Error';
+import About from './pages/About/About';
+import FicheProjet from './pages/ProjectFile/ProjectFile';
+
+import './index.css';
+
+const root = createRoot(document.getElementById('root')!);
+root.render(
+  <React.StrictMode>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="/a-propos" element={<About />} />
+          <Route path="/fiche-projet/:id" element={<FicheProjet />} />
+          <Route path="*" element={<Error />} />
+        </Route>
+      </Routes>
+    </Router>
+  </React.StrictMode>,
+);
